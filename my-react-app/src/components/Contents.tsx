e6c698bc0e61779af85ebd4cb9e778602c3f2db5
Adebayo Oladayo <matthewadebayo392@gmail.com>
2024-07-10 22:49:24 +0100

    my message1

--- my-react-app/src/components/Contents.tsx
+++ my-react-app/src/components/Contents.tsx
@@ -2,7 +2,7 @@ import messager from '../assets/icons/messenger .png'
 import '../styles/Content.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import pc1 from '../assets/pcs/pro 16.jpeg'
+import pc1 from '../assets/pcs/pc01.jpeg'
 import pc5 from '../assets/pcs/pro 16.jpeg'
 import pc2 from '../assets/pcs/samsung book3.jpeg'
 import ts from '../assets/pcs/topsell.png'
@@ -45,8 +45,8 @@ function Contents() {
             <div className="deal-contents-items">
                 <img id='hot-deals' src={pc2} alt="" />
                 <img id='heart1' src={heart} alt="l" />
-                <p className='content-text'>Dell Latitude 3420-14'
-                <p>Gen Intel Core i5</p>
+                <p className='content-text'>Apple MacBook Pro 16'
+                <p>1TB - 10 Cores M1 Max</p>
                 </p>
                 <p className='price'>$659</p>
 
@@ -58,7 +58,7 @@ function Contents() {
             <div className="deal-contents-items">
                 <img id='hot-deals' src={pc3} alt="" />
                 <img id='heart2' src={heart} alt="" />
-                <p className='content-text'>Dell Latitude 3420-14'
+                <p className='content-text'>Samsung Gallery Book 3 Pro
                 <p>Gen Intel Core i5</p>
                 </p>
                 
@@ -85,7 +85,7 @@ function Contents() {
             <div className="deal-contents-items">
                 <img id='hot-deals' src={pc5} alt="" />
                 <img id='heart4' src={heart} alt="" />
-                <p className='content-text'>Dell Latitude 3420-14'
+                <p className='content-text'>Hp Latitude 3420-14'
                 <p>Gen Intel Core i5</p>
                 </p>
                 <p className='price'>$659</p>
@@ -106,10 +106,10 @@ function Contents() {
             <div className="deal-contents-items">
                 <img id='hot-deals' src={pc1}  alt="" />
                 <img id='heart' src={heart} alt="" />
-                <p className='content-text'>Dell Latitude 3420-14'
-                <p>Gen Intel Core i5</p>
+                <p className='content-text'>Apple MacBook Pro 14"
+                <p>Apple MacBook Pro 14"</p>
                 </p>
-                <p className='price'>$659</p>
+                <p className='price'>$1,943</p>
 
                 <div className="cart-btn">
                     <button className='btn'>ADD TO CART</button>
@@ -119,10 +119,10 @@ function Contents() {
             <div className="deal-contents-items">
                 <img id='hot-deals' src={pc2} alt="" />
                 <img id='heart1' src={heart} alt="l" />
-                <p className='content-text'>Dell Latitude 3420-14'
-                <p>Gen Intel Core i5</p>
+                <p className='content-text'>Apple Macbook Pro 16'
+                <p>1TB-10 Cores M1 Max</p>
                 </p>
-                <p className='price'>$659</p>
+                <p className='price'>$3,670</p>
 
                 <div className="cart-btn">
                     <button className='btn'>ADD TO CART</button>
@@ -132,11 +132,11 @@ function Contents() {
             <div className="deal-contents-items">
                 <img id='hot-deals' src={pc3} alt="" />
                 <img id='heart2' src={heart} alt="" />
-                <p className='content-text'>Dell Latitude 3420-14'
-                <p>Gen Intel Core i5</p>
+                <p className='content-text'>Apple MacBook Air 13' 
+                <p>256GB SSD/ 8-Core GPU</p>
                 </p>
                 
-                <p className='price'>$659</p>
+                <p className='price'>$1,656</p>
 
                 <div className="cart-btn">
                     <button className='btn'>ADD TO CART</button>
@@ -146,10 +146,10 @@ function Contents() {
             <div className="deal-contents-items">
                 <img id='hot-deals' src={pc4} alt="" />
                 <img id='heart3' src={heart} alt="" />
-                <p className='content-text'>Dell Latitude 3420-14'
-                <p>Gen Intel Core i5</p>
+                <p className='content-text'>Apple MacBook Pro 16.2"
+                <p>1TB-10 Cores - M1 Max</p>
                 </p>
-                <p className='price'>$659</p>
+                <p className='price'>$3,732</p>
 
                 <div className="cart-btn">
                     <button className='btn'>ADD TO CART</button>
@@ -179,10 +179,10 @@ function Contents() {
             <div className="deal-contents-items">
                 <img id='hot-deals' src={pc1}  alt="" />
                 <img id='heart' src={heart} alt="" />
-                <p className='content-text'>Dell Latitude 3420-14'
-                <p>Gen Intel Core i5</p>
+                <p className='content-text'>DELL Latitude 7370
+                <p>Intel Core 15 8GB RAM, SSD</p>
                 </p>
-                <p className='price'>$659</p>
+                <p className='price'>$543</p>
 
                 <div className="cart-btn">
                     <button className='btn'>ADD TO CART</button>
@@ -192,10 +192,10 @@ function Contents() {
             <div className="deal-contents-items">
                 <img id='hot-deals' src={pc2} alt="" />
                 <img id='heart1' src={heart} alt="l" />
-                <p className='content-text'>Dell Latitude 3420-14'
-                <p>Gen Intel Core i5</p>
+                <p className='content-text'>DELL LATITUDE 3190 2in1 X360
+                <p>8GB RAM, 128GB SSD</p>
                 </p>
-                <p className='price'>$659</p>
+                <p className='price'>$370</p>
 
                 <div className="cart-btn">
                     <button className='btn'>ADD TO CART</button>
@@ -205,11 +205,11 @@ function Contents() {
             <div className="deal-contents-items">
                 <img id='hot-deals' src={pc3} alt="" />
                 <img id='heart2' src={heart} alt="" />
-                <p className='content-text'>Dell Latitude 3420-14'
-                <p>Gen Intel Core i5</p>
+                <p className='content-text'>DELL Latitude 7490 TOUCH 1
+                <p>TB SSD/16GB RAM</p>
                 </p>
                 
-                <p className='price'>$659</p>
+                <p className='price'>$1,156</p>
 
                 <div className="cart-btn">
                     <button className='btn'>ADD TO CART</button>
@@ -219,10 +219,10 @@ function Contents() {
             <div className="deal-contents-items">
                 <img id='hot-deals' src={pc4} alt="" />
                 <img id='heart3' src={heart} alt="" />
-                <p className='content-text'>Dell Latitude 3420-14'
-                <p>Gen Intel Core i5</p>
+                <p className='content-text'>DELL Alienware M18 R1
+                <p>4TB/64GB 19 13th Gen</p>
                 </p>
-                <p className='price'>$659</p>
+                <p className='price'>$5,932</p>
 
                 <div className="cart-btn">
                     <button className='btn'>ADD TO CART</button>
@@ -252,10 +252,10 @@ function Contents() {
             <div className="deal-contents-items">
                 <img id='hot-deals' src={pc1}  alt="" />
                 <img id='heart' src={heart} alt="" />
-                <p className='content-text'>Dell Latitude 3420-14'
-                <p>Gen Intel Core i5</p>
+                <p className='content-text'>Lenovo ThinkPad P16s G1
+                <p>i7-1280P 1.80 GHz</p>
                 </p>
-                <p className='price'>$659</p>
+                <p className='price'>$1,543</p>
 
                 <div className="cart-btn">
                     <button className='btn'>ADD TO CART</button>
@@ -265,10 +265,10 @@ function Contents() {
             <div className="deal-contents-items">
                 <img id='hot-deals' src={pc2} alt="" />
                 <img id='heart1' src={heart} alt="l" />
-                <p className='content-text'>Dell Latitude 3420-14'
-                <p>Gen Intel Core i5</p>
+                <p className='content-text'>Lenovo Ideapad Intel 
+                <p>256 SSD 4GB RAM</p>
                 </p>
-                <p className='price'>$659</p>
+                <p className='price'>$$1170</p>
 
                 <div className="cart-btn">
                     <button className='btn'>ADD TO CART</button>
@@ -278,11 +278,11 @@ function Contents() {
             <div className="deal-contents-items">
                 <img id='hot-deals' src={pc3} alt="" />
                 <img id='heart2' src={heart} alt="" />
-                <p className='content-text'>Dell Latitude 3420-14'
-                <p>Gen Intel Core i5</p>
+                <p className='content-text'>Lenovo V14 AMD RYZEN
+                <p>12GB RAM 1TB HDD</p>
                 </p>
                 
-                <p className='price'>$659</p>
+                <p className='price'>$966</p>
 
                 <div className="cart-btn">
                     <button className='btn'>ADD TO CART</button>
@@ -292,10 +292,10 @@ function Contents() {
             <div className="deal-contents-items">
                 <img id='hot-deals' src={pc4} alt="" />
                 <img id='heart3' src={heart} alt="" />
-                <p className='content-text'>Dell Latitude 3420-14'
-                <p>Gen Intel Core i5</p>
+                <p className='content-text'>Lenovo Thinkpad X1 
+                <p>Gen 11 Core 17-1365u</p>
                 </p>
-                <p className='price'>$659</p>
+                <p className='price'>$5,932</p>
 
                 <div className="cart-btn">
                     <button className='btn'>ADD TO CART</button>
